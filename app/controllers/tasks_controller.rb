bbc44c919309028d87438db009dd05f491a7d5b4
jiro.kirameki <edcated2111@aurora.ocn.ne.jp>
2018-02-13 12:34:28 +0000

    taihi

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,54 +1,24 @@
 class TasksController < ApplicationController
-  def index
-    @tasks=Task.all.page(params[:page])
-  end
-  
-  def show
-    @task=Task.find(params[:id])
-  end
-  
-  def new
-    @task=Task.new
-  end
-  
+  before_action :require_user_logged_in
   def create
-    @task=Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success]='Taskが正常に投稿されました'
-      redirect_to @task
-    else
-      flash.now[:danger]='Taskが投稿されませんでした'
-      render :new
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
+      else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
-  
-  def edit
-    @task=Task.find(params[:id])
-  end
-  
-  def update
-    @task=Task.find(params[:id])
-    
-    if @task.update(task_params)
-      flash[:success]='Taskが正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger]='Taskが更新されませんでした'
-      render :edit
-    end
-  end
-  
-  def destroy
-    @task=Task.find(params[:id])
-    @task.destroy
-    
-    flash[:success]='Taskは正常に削除されました'
-    redirect_to tasks_url
-  end
-  
-end
 
-def task_params
-  params.require(:task).permit(:content, :status)
+  def destroy
+  end
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'tasks/create'
+
+  get 'tasks/destroy'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:index, :show, :new, :create]
   resources :tasks
 end
